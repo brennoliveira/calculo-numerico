add29013e7ebd6a742e51162f39f6294e0e65f84
Brenno Santos Oliveria <brenno77.s.oliveira@gmail.com>
2023-03-05 17:49:45 -0300

    add Tests

--- python/methods/main.py
+++ python/methods/main.py
@@ -1,4 +1,10 @@
 import bisection as bi
+import newton as ne
 
-raiz = bi.bissecao(bi.f, 1, 3, 0.001)
+raiz = bi.bissecao(bi.f, 0, 1, 0.001)
+print(raiz)
+
+print("*-"*20)
+
+raiz = ne.newton(ne.f, ne.fd, 0, 0.001)
 print(raiz)
