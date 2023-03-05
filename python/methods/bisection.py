3f9e52882c0ae2ff437ed50fa9ad211005bf4822
Brenno Santos Oliveria <brenno77.s.oliveira@gmail.com>
2023-03-05 19:06:44 -0300

    update Parameter

--- python/methods/bisection.py
+++ python/methods/bisection.py
@@ -1,11 +1,11 @@
-def bissecao(f, a, b, tol):
+def bissecao(f, a, b, prec):
     """
-    Encontra a raiz da função f no intervalo [a, b] com tolerância de erro tol.
+    Encontra a raiz da função f no intervalo [a, b] com precisão prec.
     """
     if f(a) * f(b) > 0:
         raise ValueError("A função não muda de sinal no intervalo dado.")
     i = 0
-    while b - a > tol:
+    while b - a > prec:
         i += 1
         c = (a + b) / 2
         if f(c) == 0:
