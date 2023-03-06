046b4bbed3270b5ded72fec47f7657bc0ea936c2
Brenno Santos Oliveria <brenno77.s.oliveira@gmail.com>
2023-03-05 22:06:02 -0300

    update Bisection

--- python/methods/bisection.py
+++ python/methods/bisection.py
@@ -8,10 +8,10 @@ def bissecao(f, a, b, prec):
     if f(a) * f(b) > 0:
         raise ValueError("A função não muda de sinal no intervalo dado.")
     i = 0
-    while b - a > prec:
+    while abs(b - a) > prec:
         i += 1
         c = (a + b) / 2
-        if f(c) == 0:
+        if abs(f(c)) < prec:
             print(f"iterações: {i}")
             return c
         elif f(a) * f(c) < 0:
