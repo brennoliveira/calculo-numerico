15aec88e94e172c52d81f63962b20f3f0ff41719
Brenno Santos Oliveria <brenno77.s.oliveira@gmail.com>
2023-03-05 21:17:17 -0300

    add Secant Method

--- python/methods/main.py
+++ python/methods/main.py
@@ -1,5 +1,6 @@
 import bisection as bi
 import newton as ne
+import secant as se
 
 raiz = bi.bissecao(bi.f, 0, 1, 0.001)
 print(raiz)
@@ -7,4 +8,9 @@ print(raiz)
 print("*-"*20)
 
 raiz = ne.newton(ne.f, ne.fd, 0, 0.001)
+print(raiz)
+
+print("*-"*20)
+
+raiz = se.secant(se.f, 0, 1, 0.001)
 print(raiz)
